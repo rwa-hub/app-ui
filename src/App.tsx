61e5d0c4a404efee85c5695a778c350d545b1b38
renancorreadev <skynancemusic@gmail.com>
2025-03-14 00:21:18 -0300

    update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,40 @@
-import React from "react";
-import { Dashboard } from "@/pages";
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+import { Dashboard } from "@/shared/Layout";
+import { EventList } from "@/pages/EventList";
+import { Token } from "@/pages/Token";
+import { Agents } from "@/pages/Agents";
+import { ApproveBuyer } from "@/pages/ApproveBuyer";
+import { KYC } from "@/pages/KYC";
+import { Home } from "@/pages/Home";
+import { About } from "@/pages/SidebarPages/About";
+import { RWA } from "@/pages/SidebarPages/RWA";
+import { Docs } from "./pages/SidebarPages";
 
-export default function Page() {
+
+function App() {
   return (
-    <React.Fragment>
-      <Dashboard />
-    </React.Fragment>
-  )
+    <Router>
+      <Routes>
+        {/* Redireciona para Home no primeiro acesso */}
+        <Route path="/" element={<Home />} />
+
+        {/* Dashboard é o layout principal que contém Sidebar e Header */}
+        <Route path="/dashboard" element={<Dashboard />}>
+          <Route path="rwa" element={<RWA />} />
+          <Route path="about" element={<About />} />
+          <Route path="docs" element={<Docs />} />
+          <Route path="events" element={<EventList />} />
+          <Route path="tokens" element={<Token />} />
+          <Route path="agents" element={<Agents />} />
+          <Route path="approve-buyer" element={<ApproveBuyer />} />
+          <Route path="kyc" element={<KYC />} />
+        </Route>
+
+        {/* Página 404 caso a rota não exista */}
+        <Route path="*" element={<Navigate to="/dashboard/home" />} />
+      </Routes>
+    </Router>
+  );
 }
+
+export default App;
